530a0b18cfc1be9273b07e58e1e9003f98075888
Namyunsuk <yunsuk0328@naver.com>
2025-04-18 20:06:38 +0900

    style: ktlint 적용

--- presentation/src/test/java/com/analysis/presentation/component/GgzzTopAppBarTest.kt
+++ presentation/src/test/java/com/analysis/presentation/component/GgzzTopAppBarTest.kt
@@ -1,7 +1,5 @@
 package com.analysis.presentation.component
 
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 import androidx.compose.material3.IconButton
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -11,6 +9,8 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
 class GgzzTopAppBarTest {
