dc77630a14ae0ba63c197cea4b456caed8707be8
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-04-18 18:20:12 +0900

    design: UI 리소스 추가 (#8)
    * feat: font파일 추가
    
    * feat: color 추가
    
    * feat: typhography 추가
    
    * feat: theme 추가
    
    * chore: ktlint 함수 체이닝 포매팅 방식 수정
    
    * style: ktlint적용
    
    * rename: editorconfig 네이밍 변경
    
    * chore: app name변경

--- presentation/src/main/java/com/analysis/presentation/theme/Color.kt
+++ presentation/src/main/java/com/analysis/presentation/theme/Color.kt
@@ -2,10 +2,32 @@ package com.analysis.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+internal val Red600 = Color(0xFFF44336)
+internal val Red500 = Color(0xFFEF4F4B)
+internal val Red400 = Color(0xFFF15958)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+internal val Green400 = Color(0xFF67D481)
+
+internal val Yellow400 = Color(0xFFFFE364)
+
+internal val Blue300 = Color(0xFF32404E)
+internal val Blue200 = Color(0xFF455668)
+
+internal val Purple700 = Color(0xFF6C7BFF)
+internal val Purple500 = Color(0xFF9DA7FB)
+internal val Purple300 = Color(0xFFCAC4D0)
+internal val Purple200 = Color(0xFFF7F8FF)
+
+internal val Gray1000 = Color(0xFF222222)
+internal val Gray900 = Color(0xFF3E3E3E)
+internal val Gray800 = Color(0xFF51595F)
+internal val Gray700 = Color(0xFF949494)
+internal val Gray600 = Color(0xFFBABABA)
+internal val Gray500 = Color(0xFFD2D2D2)
+internal val Gray400 = Color(0xFFE2E2E2)
+internal val Gray300 = Color(0xFFEDEFF5)
+internal val Gray200 = Color(0xFFF2F2F6)
+internal val Gray100 = Color(0xFFF3F3F7)
+
+internal val White = Color(0xFFFFFFFF)
+internal val Black = Color(0xFF000000)
--- presentation/src/main/java/com/analysis/presentation/theme/Theme.kt
+++ presentation/src/main/java/com/analysis/presentation/theme/Theme.kt
@@ -1,59 +1,51 @@
 package com.analysis.presentation.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme =
-    darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80,
-    )
+internal object GgzzColorScheme {
+    val primary: Color = Purple700
+}
 
-private val LightColorScheme =
-    lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40,
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
-    )
+private val LocalColorScheme = staticCompositionLocalOf { GgzzColorScheme }
+
+private val LocalTypography = staticCompositionLocalOf { GgzzTypography }
+
+internal object GgzzTheme {
+    val colorScheme: GgzzColorScheme
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalColorScheme.current
+
+    val typography: GgzzTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTypography.current
+}
 
 @Composable
-fun GgzzTheme(
+internal fun GgzzTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    typography: GgzzTypography = GgzzTheme.typography,
     content: @Composable () -> Unit,
 ) {
     val colorScheme =
         when {
-            dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                val context = LocalContext.current
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-            }
-
-            darkTheme -> DarkColorScheme
-            else -> LightColorScheme
+            darkTheme -> GgzzColorScheme
+            else -> GgzzColorScheme
         }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content,
-    )
+    CompositionLocalProvider(
+        LocalColorScheme provides colorScheme,
+        LocalTypography provides typography,
+    ) {
+        MaterialTheme(
+            content = content,
+        )
+    }
 }
