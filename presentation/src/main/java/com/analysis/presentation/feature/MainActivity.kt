206d9212030011bae35be06e1bc0a9f527bc0699
Namyunsuk <yunsuk0328@naver.com>
2025-04-18 21:34:41 +0900

    feat: MainScreen구현

--- presentation/src/main/java/com/analysis/presentation/feature/MainActivity.kt
+++ presentation/src/main/java/com/analysis/presentation/feature/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.analysis.presentation.feature.main.MainScreen
+import com.analysis.presentation.navigation.GgzzNavController
 import com.analysis.presentation.theme.GgzzTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,33 +14,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
+            val GgzzNavController = GgzzNavController(navController)
+
             GgzzTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
-                }
+                MainScreen(GgzzNavController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GgzzTheme {
-        Greeting("Android")
     }
 }
---
+++ presentation/src/main/java/com/analysis/presentation/feature/main/MainScreen.kt
@@ -0,0 +1,88 @@
+package com.analysis.presentation.feature.main
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.analysis.presentation.R
+import com.analysis.presentation.component.GgzzNavigationBar
+import com.analysis.presentation.navigation.GgzzNavController
+import com.analysis.presentation.navigation.GgzzNavHost
+import com.analysis.presentation.navigation.NavTab
+import com.analysis.presentation.theme.GgzzTheme
+import kotlinx.coroutines.launch
+
+@Composable
+internal fun MainScreen(
+    navController: GgzzNavController,
+) {
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
+    val snackBarHostState = remember { SnackbarHostState() }
+    val showErrorSnackbar: (Throwable) -> Unit = {
+        coroutineScope.launch {
+            snackBarHostState.showSnackbar(context.getString(R.string.unknown_error_snackbar))
+        }
+    }
+
+    MainContent(navController, snackBarHostState, showErrorSnackbar)
+}
+
+@Composable
+private fun MainContent(
+    navController: GgzzNavController,
+    snackBarHostState: SnackbarHostState,
+    showErrorSnackbar: (Throwable) -> Unit,
+) {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .navigationBarsPadding(),
+        snackbarHost = { SnackbarHost(snackBarHostState) },
+        bottomBar = {
+            GgzzNavigationBar(
+                isVisible = navController.isNavigationBarVisible,
+                currentTab = navController.currentTab,
+                tabs = NavTab.entries,
+                onTabClick = {
+                    navController.navigate(it)
+                },
+            )
+        },
+    ) { innerPadding ->
+        GgzzNavHost(
+            modifier = Modifier.padding(innerPadding),
+            navController = navController,
+            startDestination = navController.startDestination,
+            showErrorSnackbar = showErrorSnackbar,
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun MainContentPreview() {
+    val navController = rememberNavController()
+    val jipmoNavController = GgzzNavController(navController)
+    val snackBarHostState = remember { SnackbarHostState() }
+
+    GgzzTheme {
+        MainContent(
+            navController = jipmoNavController,
+            snackBarHostState = snackBarHostState,
+            showErrorSnackbar = {},
+        )
+    }
+}
