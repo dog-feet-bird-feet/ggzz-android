b89ac03064613138c175202b2adfb10cd7459bda
Namyunsuk <yunsuk0328@naver.com>
2025-04-18 21:28:03 +0900

    feat: BottomNavigationBar 구현

--- presentation/src/main/java/com/analysis/presentation/feature/MainActivity.kt
+++ presentation/src/main/java/com/analysis/presentation/feature/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.analysis.presentation
+package com.analysis.presentation.feature
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
---
+++ presentation/src/main/java/com/analysis/presentation/navigation/GgzzNavHost.kt
@@ -0,0 +1,26 @@
+package com.analysis.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import com.analysis.presentation.feature.history.navigation.historyNavGraph
+import com.analysis.presentation.feature.home.navigation.homeNavGraph
+import com.analysis.presentation.feature.setting.navigation.settingNavGraph
+
+@Composable
+internal fun GgzzNavHost(
+    modifier: Modifier = Modifier,
+    navController: GgzzNavController,
+    startDestination: NavRoute,
+    showErrorSnackbar: (Throwable) -> Unit,
+) {
+    NavHost(
+        navController = navController.navController,
+        startDestination = startDestination,
+        modifier = modifier,
+    ) {
+        homeNavGraph()
+        historyNavGraph()
+        settingNavGraph()
+    }
+}
