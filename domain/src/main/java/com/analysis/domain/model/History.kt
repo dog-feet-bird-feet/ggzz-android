a039a4c05e5626f22fab1d443a65159155654ba2
Namyunsuk <yunsuk0328@naver.com>
2025-05-01 20:30:14 +0900

    refactor: nullable제거

--- domain/src/main/java/com/analysis/domain/model/History.kt
+++ domain/src/main/java/com/analysis/domain/model/History.kt
@@ -5,6 +5,6 @@ import java.time.LocalDateTime
 data class History(
     val id: String,
     val title: String,
-    val createdAt: LocalDateTime?,
+    val createdAt: LocalDateTime,
     val verificationImgUrl: String,
 )
