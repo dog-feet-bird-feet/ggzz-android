21abcdfe4334615b629ff0c039ce46c1f19741ed
Namyunsuk <yunsuk0328@naver.com>
2025-05-01 17:04:37 +0900

    feat: 히스토리 항목 구현

--- presentation/src/main/java/com/analysis/presentation/feature/verify/component/HandWritingImageItemCard.kt
+++ presentation/src/main/java/com/analysis/presentation/feature/verify/component/HandWritingImageItemCard.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
+import coil3.compose.rememberAsyncImagePainter
 import com.analysis.presentation.R
 
 @Composable
