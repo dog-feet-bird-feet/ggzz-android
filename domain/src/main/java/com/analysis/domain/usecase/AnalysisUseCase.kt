bef4e6c9c767d9e9fa943a23e065fe78876cf0f7
Namyunsuk <yunsuk0328@naver.com>
2025-05-16 16:16:51 +0900

    feat: usecase 작성

---
+++ domain/src/main/java/com/analysis/domain/usecase/AnalysisUseCase.kt
@@ -0,0 +1,34 @@
+package com.analysis.domain.usecase
+
+import com.analysis.domain.model.AnalysisResult
+import com.analysis.domain.repository.AnalysisRepository
+import com.analysis.domain.repository.UploadRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import okhttp3.MultipartBody
+import javax.inject.Inject
+
+class AnalysisUseCase @Inject constructor(
+    private val uploadRepository: UploadRepository,
+    private val analysisRepository: AnalysisRepository,
+) {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    operator fun invoke(
+        comparisons: List<MultipartBody.Part>,
+        verification: MultipartBody.Part,
+    ): Flow<AnalysisResult> {
+        return uploadRepository
+            .saveComparisons(comparisons)
+            .combine(uploadRepository.saveVerification(verification)) { list, single ->
+                list to single
+            }
+            .flatMapLatest { (comparisonUrls, verificationUrl) ->
+                analysisRepository.executeAnalysis(
+                    verificationImageUrl = verificationUrl,
+                    comparisonImageUrls = comparisonUrls
+                )
+            }
+    }
+}
--- domain/src/main/java/com/analysis/domain/usecase/FetchHistoriesUseCase.kt
+++ domain/src/main/java/com/analysis/domain/usecase/FetchHistoriesUseCase.kt
@@ -1,16 +1,21 @@
 package com.analysis.domain.usecase
 
+import com.analysis.domain.model.AnalysisResult
 import com.analysis.domain.model.History
+import com.analysis.domain.repository.AnalysisRepository
 import com.analysis.domain.repository.HistoryRepository
+import com.analysis.domain.repository.UploadRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import okhttp3.MultipartBody
 import javax.inject.Inject
 
-class FetchHistoriesUseCase
-    @Inject
-    constructor(
-        private val historyRepository: HistoryRepository,
-    ) {
-        operator fun invoke(): Flow<List<History>> {
-            return historyRepository.fetchHistories()
-        }
+class FetchHistoriesUseCase @Inject constructor(
+    private val historyRepository: HistoryRepository,
+) {
+    operator fun invoke(): Flow<List<History>> {
+        return historyRepository.fetchHistories()
     }
+}
