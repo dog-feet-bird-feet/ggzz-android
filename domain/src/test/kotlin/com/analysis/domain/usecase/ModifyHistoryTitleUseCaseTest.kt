b7b6b5fb888f2dd62d158f28a07c7c7f522ccfbc
Namyunsuk <yunsuk0328@naver.com>
2025-06-05 02:46:15 +0900

    test: ModifyHistoryTitleUseCaseTest 작성

---
+++ domain/src/test/kotlin/com/analysis/domain/usecase/ModifyHistoryTitleUseCaseTest.kt
@@ -0,0 +1,31 @@
+package com.analysis.domain.usecase
+
+import com.analysis.domain.repository.HistoryRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class ModifyHistoryTitleUseCaseTest {
+    private val historyRepository: HistoryRepository = mockk()
+    private val modifyHistoryTitleUseCase: ModifyHistoryTitleUseCase = ModifyHistoryTitleUseCase(historyRepository)
+
+    @Test
+    @DisplayName("히스토리 제목을 수정한다")
+    fun modifyHistoryTitle() {
+        runTest {
+            // given
+            coEvery { historyRepository.modifyHistoryTitle("1","test") } returns flowOf(Unit)
+
+            // when
+            val actual = modifyHistoryTitleUseCase("1","test")
+
+            // then
+            assertThat(actual.first()).isEqualTo(Unit)
+        }
+    }
+}
