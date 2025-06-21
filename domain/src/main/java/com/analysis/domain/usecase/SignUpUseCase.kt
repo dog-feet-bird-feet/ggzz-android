7051e5b12e358f88cb982483b9bfecdb7884e415
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-06-21 23:05:20 +0900

    fix: 회원가입 시 액세스 토큰저장  (#49)
    * fix: 회원가입 시 액세스 토큰을 저장하도록 수정
    
    * style: ktlint 적용

--- domain/src/main/java/com/analysis/domain/usecase/SignUpUseCase.kt
+++ domain/src/main/java/com/analysis/domain/usecase/SignUpUseCase.kt
@@ -1,7 +1,10 @@
 package com.analysis.domain.usecase
 
 import com.analysis.domain.repository.LoginRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class SignUpUseCase
@@ -9,10 +12,17 @@ class SignUpUseCase
     constructor(
         private val loginRepository: LoginRepository,
     ) {
+        @OptIn(ExperimentalCoroutinesApi::class)
         operator fun invoke(
             email: String,
             password: String,
         ): Flow<Boolean> {
-            return loginRepository.signUp(email, password)
+            return loginRepository.signUp(email, password).flatMapLatest { isSignUpSuccess ->
+                if (isSignUpSuccess) {
+                    loginRepository.login(email, password)
+                } else {
+                    flowOf(false)
+                }
+            }
         }
     }
