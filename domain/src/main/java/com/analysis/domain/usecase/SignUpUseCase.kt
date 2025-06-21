e52b7f8d877b3615d3c23118e170aff3fd9135dd
Namyunsuk <yunsuk0328@naver.com>
2025-06-21 21:20:59 +0900

    style: ktlint 적용

--- domain/src/main/java/com/analysis/domain/usecase/SignUpUseCase.kt
+++ domain/src/main/java/com/analysis/domain/usecase/SignUpUseCase.kt
@@ -8,21 +8,21 @@ import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class SignUpUseCase
-@Inject
-constructor(
-    private val loginRepository: LoginRepository,
-) {
-    @OptIn(ExperimentalCoroutinesApi::class)
-    operator fun invoke(
-        email: String,
-        password: String,
-    ): Flow<Boolean> {
-        return loginRepository.signUp(email, password).flatMapLatest { isSignUpSuccess ->
-            if (isSignUpSuccess) {
-                loginRepository.login(email, password)
-            } else {
-                flowOf(false)
+    @Inject
+    constructor(
+        private val loginRepository: LoginRepository,
+    ) {
+        @OptIn(ExperimentalCoroutinesApi::class)
+        operator fun invoke(
+            email: String,
+            password: String,
+        ): Flow<Boolean> {
+            return loginRepository.signUp(email, password).flatMapLatest { isSignUpSuccess ->
+                if (isSignUpSuccess) {
+                    loginRepository.login(email, password)
+                } else {
+                    flowOf(false)
+                }
             }
         }
     }
-}
