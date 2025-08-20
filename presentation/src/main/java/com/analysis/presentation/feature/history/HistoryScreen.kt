3ecc2f3b7adbd1ca0a8893d8bb049fc896159546
Namyunsuk <yunsuk0328@naver.com>
2025-08-21 02:54:04 +0900

    refactor: HistoryScreen Scaffold 제거

--- presentation/src/main/java/com/analysis/presentation/feature/history/HistoryScreen.kt
+++ presentation/src/main/java/com/analysis/presentation/feature/history/HistoryScreen.kt
@@ -1,21 +1,26 @@
 package com.analysis.presentation.feature.history
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.analysis.domain.model.History
 import com.analysis.presentation.R
 import com.analysis.presentation.component.GgzzTopAppBar
 import com.analysis.presentation.feature.history.component.HistoryItemCard
@@ -31,15 +36,11 @@ internal fun HistoryScreen(
 ) {
     LaunchedEffect(Unit) {
         viewModel.error.collectLatest { showErrorSnackBar(it) }
-    }
 
-    LaunchedEffect(Unit) {
         viewModel.isModifySuccess.collectLatest {
             if (it) viewModel.fetchHistories()
         }
-    }
 
-    LaunchedEffect(Unit) {
         viewModel.isRemoveSuccess.collectLatest {
             if (it) viewModel.fetchHistories()
         }
@@ -47,39 +48,73 @@ internal fun HistoryScreen(
 
     val histories by viewModel.histories.collectAsStateWithLifecycle()
 
-    Scaffold(
-        topBar = {
-            GgzzTopAppBar(
-                title = stringResource(R.string.history_top_app_bar_title),
-            )
-        },
-        containerColor = Gray100,
-    ) { innerPadding ->
-        LazyColumn(
-            modifier = Modifier
-                .padding(defaultPadding)
-                .padding(innerPadding)
-                .padding(horizontal = 20.dp)
-                .padding(top = 20.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            items(
-                items = histories,
-                key = { it.id },
-            ) {
-                HistoryItemCard(
-                    history = it,
-                    { navigateToResult(it) },
-                    { id, newTitle ->
-                        viewModel.modifyHistoryTitle(id, newTitle)
-                    },
-                    { viewModel.removeHistory(it) },
-                )
-            }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Gray100)
+            .systemBarsPadding()
+            .padding(defaultPadding),
+    ) {
+        GgzzTopAppBar(
+            title = stringResource(R.string.history_top_app_bar_title),
+        )
+        HistoryList(
+            histories,
+            navigateToResult,
+            modifyHistoryTitle = { id, newTitle -> viewModel.modifyHistoryTitle(id, newTitle) },
+            removeHistory = { id -> viewModel.removeHistory(id) },
+        )
+    }
+}
 
-            item {
-                Spacer(modifier = Modifier.height(20.dp))
-            }
+@Composable
+private fun HistoryList(
+    histories: List<History>,
+    navigateToResult: (String) -> Unit,
+    modifyHistoryTitle: (String, String) -> Unit,
+    removeHistory: (String) -> Unit,
+) {
+    LazyColumn(
+        modifier = Modifier
+            .padding(horizontal = 20.dp)
+            .padding(top = 20.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        items(
+            items = histories,
+            key = { it.id },
+        ) {
+            HistoryItemCard(
+                history = it,
+                { id -> navigateToResult(id) },
+                { id, newTitle ->
+                    modifyHistoryTitle(id, newTitle)
+                },
+                { id -> removeHistory(id) },
+            )
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(20.dp))
         }
     }
+}
+
+@Preview
+@Composable
+private fun HistoryListPreview() {
+    val histories = (1..10).map {
+        History(
+            id = it.toString(),
+            title = "History 1",
+            createdAt = "2023-01-01",
+            verificationImgUrl = ""
+        )
+    }
+    HistoryList(
+        histories = histories,
+        navigateToResult = {},
+        modifyHistoryTitle = { _, _ -> },
+        removeHistory = {}
+    )
 }
--- presentation/src/main/java/com/analysis/presentation/feature/home/HomeScreen.kt
+++ presentation/src/main/java/com/analysis/presentation/feature/home/HomeScreen.kt
@@ -10,9 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -37,7 +34,8 @@ internal fun HomeScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(Gray100)
-            .systemBarsPadding(),
+            .systemBarsPadding()
+            .padding(defaultPadding),
     ) {
         GgzzTopAppBar(
             title = stringResource(R.string.home_top_app_bar_title),
